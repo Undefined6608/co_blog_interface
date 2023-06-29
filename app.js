5eea784b03089aa8ddfdee211ccf581153107bf2
15506 <15506531157@163.com>
2023-06-29 14:10:50 +0800

    Undefined6608

--- app.js
+++ app.js
@@ -5,17 +5,37 @@ const express = require('express');
 const {logger, sessionSecret} = require("./config/connfig.default");
 // 导入morgan
 const morgan = require('morgan');
-// 导入cors
-const cors = require('cors');
 // 导入router
 const router = require('./router');
 // 导入错误处理中间件
 const errorHandler = require('./middleware/error-handler');
+// 导入cors
+const cors = require('cors');
 // 导入session
 const session = require('express-session');
 /*****************创建配置信息**********************/
 // 创建express对象
 const app = express();
+// 定义允许跨域请求的请求源地址列表
+const allowedOrigins = [
+    'http://192.168.126.1:3000',
+    'http://localhost:3000'
+];
+
+// 挂载cors实现跨域
+app.use(cors({
+    origin: (origin, callback) => {
+        // 检查请求的来源是否在允许的列表中
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('不允许的来源'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的请求方法
+    credentials: true // 允许携带凭证（如 Cookie）
+}));
+
 // 挂载session
 app.use(session({
     secret: sessionSecret,
@@ -32,9 +52,7 @@ app.use(morgan('dev'));
 const PORT = process.env.PORT || 4001;
 // 配置和挂载解析body请求体方法
 app.use(express.json());
-app.use(express.urlencoded({extended:false,limit:'20mb'}));
-// 挂载cors用于跨域
-app.use(cors());
+app.use(express.urlencoded({extended: false, limit: '20mb'}));
 // 挂载router,所有的请求都以/api开头
 app.use('/api', router);
 // 挂载统一处理服务器错误的中间件
